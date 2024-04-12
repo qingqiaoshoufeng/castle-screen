70cc2372a5f0117cdf60ab7168d29059233704ef
yuzhiqiang <yuzhiqiang2@hxdi.com>
2024-04-12 11:08:32 +0800

    feat: 添加chalk-theme模块

---
+++ packages/theme-chalk/gulpfile.mjs
@@ -0,0 +1,32 @@
+import { series, src, dest } from 'gulp'
+import gulpSass from 'gulp-sass' // 处理sass
+import dartSass from 'sass'
+import autoprefixer from 'gulp-autoprefixer' // 添加前缀
+import cleanCSS from 'gulp-clean-css' // 压缩css
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function compile() { // 处理scss文件
+    const sass = gulpSass(dartSass)
+    return src(path.resolve(__dirname, './src/*.scss'))
+        .pipe(sass.sync())
+        .pipe(autoprefixer({}))
+        .pipe(cleanCSS())
+        .pipe(dest('./dist/css'))
+}
+// function copyfont() { // 拷贝字体样式
+//     return src(path.resolve(__dirname, './src/fonts/**')).pipe(cleanCSS()).pipe(dest('./dist/fonts'))
+// }
+// function copyStyle(){
+//     return src(path.resolve(__dirname,'dist/**')).pipe(dest(path.resolve(__dirname,'../../dist/theme-chalk')));
+// }
+const buildStyle = series(
+    compile,
+    // copyfont,
+    // copyStyle
+)
+export default buildStyle
