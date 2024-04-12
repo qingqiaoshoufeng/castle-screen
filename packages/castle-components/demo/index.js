6834f5c30cb257a25ad6c81e24de2128d7d47c7c
yuzhiqiang <yuzhiqiang2@hxdi.com>
2024-04-12 11:12:50 +0800

    feat: 添加demo使用生成样式的示例。。。。

--- packages/castle-components/demo/index.js
+++ packages/castle-components/demo/index.js
@@ -1,5 +1,5 @@
 import { withInstall } from '../../utils/install'
-
+import '@castle-screen/theme-chalk/dist/css/demo.css'
 import demo from './src/demo.vue'
 demo.name = 'caDemo'
 export const caDemo = withInstall(demo)
